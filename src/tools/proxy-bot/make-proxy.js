aa7e2a19242a96dd5f078c4574a1447eaa8b21a8
mk <mk@mks-mac-mini-2.lan>
2022-05-10 11:58:49 -0400

    Updates: proxy script added

--- src/tools/proxy-bot/make-proxy.js
+++ src/tools/proxy-bot/make-proxy.js
@@ -5,92 +5,23 @@ var movRegex = /(mov|mp4|mxf|mts|m4v)/i;
 var filesToCopyRegex = /(jpg|wav|png|aac|mp3)/i;
 var ffprobeToJson = require('./ffprobe-to-json');
 const { cyan, blue, yellow, magenta, gray, white, divider } = require(`../utilities/mk-utilities`);
-const { grey } = require('colors');
-
-// var transcodeFolder = async function(folder, options){
-//   var theFiles = fs.readdirSync(folder);
-//   if (options && options.proxyFolder) {
-//     var proxyFolder = path.join(proxyFolder, (path.basename(folder) + '_proxy'));
-//   } else {
-//     var proxyFolder = path.join(path.dirname(folder), (path.basename(folder) + '_proxy'));
-//   }
-//   if (!fs.existsSync(proxyFolder)) {
-//     fs.mkdirSync(proxyFolder, {recursive: true})
-//   }
-//   console.log("proxyFolder is " + proxyFolder);
-//   console.log(JSON.stringify(theFiles, null, 4));
-//   for (var i = 0; i < theFiles.length; i++) {
-//     if (movRegex.test(theFiles[i])) {
-//       console.log("this is a movie: " + theFiles[i]);
-//       await transcodeFile(
-//         path.join(folder, theFiles[i]),
-//         path.join(proxyFolder, theFiles[i]),
-//         {
-//           crfVal: (options && options.crf) ? options.crf : '23'
-//         }
-//       );
-//     } else {
-//       console.log(theFiles[i] + " doesn't seem to be a movie");
-//     }
-//   }
-// }
-
-
-
-
-var transcodeFile = async function(file, proxyPath, options){
-  await cp.spawnSync('ffmpeg', [
-    '-i', file,
-    '-c:v', 'libx264',
-    '-pix_fmt', 'yuv420p',
-    // '-vf', ('scale='+ outputWidth +':'+outputHeight ),
-    '-preset', 'slow',
-    '-crf', (options && options.crfVal) ? options.crfVal : '23',
-    '-ac', '2',
-    '-c:a', 'aac',
-    '-b:a', '128k',
-    proxyPath
-  ], {
-    stdio: [
-      0, // Use parent's stdin for child
-      'pipe', // Pipe child's stdout to parent
-      2 // Direct child's stderr to a file
-    ]
-  });
-}
-
-// function getDesiredDimensions(videoFilePath){
-//   var options = ['-v', 'error', '-print_format', 'json', '-select_streams', 'v:0', '-show_entries', 'stream=width,height'];
-//   var output = JSON.parse(ffprobetools.ffprobeSyncSimple(videoFilePath, options));
-//   var outputWidth=1920;
-//   var outputHeight=1080;
-//   console.log(output.streams[0].width + " is the width");
-//   if (output.streams[0].height && (output.streams[0].height>1080)) {
-//     console.log(videoFilePath + " has height larger than 1080: " + output.streams[0].height);
-//     outputWidth=output.streams[0].width/(output.streams[0].height/1080);
-//     console.log("making outputWidth " + outputWidth);
-//   }
-//   else if (output.streams[0].height && (output.streams[0].height==1080)) {
-//     console.log(videoFilePath + " has height of exactly 1080: " + output.streams[0].height);
-//   }
-//   else if (output.streams[0].height && (output.streams[0].height<1080)) {
-//     console.log(videoFilePath + " has height of less than 1080: " + output.streams[0].height);
-//     outputWidth=output.streams[0].width;
-//     outputHeight=output.streams[0].height;
-//   }
-//   else {
-//     console.log("something went wrong--perhaps this is not a video file");
-//   }
-//   return {outputWidth: outputWidth, outputHeight: outputHeight};
-// }
-
+var intervalToDuration = require('date-fns/intervalToDuration')
+var format = require('date-fns/format')
 
 const makeShootProxy = async function (folder, options) {
-    gray(`starting makeShootProxy with folder:\n${folder}\nand options:\n${JSON.stringify(options, null, 4)}`)
+    const jobStart = new Date()
+    gray(`starting makeShootProxy at ${format(jobStart, "yyyyMMdd.HH:mm:ss.SSS")} with folder:\n${folder}\nand options:\n${JSON.stringify(options, null, 4)}`)
     const listOfOperations = await createListOfOperations(folder)
     magenta(divider, `listOfOperations`, divider)
     gray(listOfOperations)
     const result = await performOperations(listOfOperations)
+    const jobEnd = newDate()
+    const jobDuration = intervalToDuration({
+        start: jobStart,
+        end: jobEnd
+    })
+    magenta(divider, `job took ${jobDuration} to complete`)
+    gray(format(jobDuration, "HH:mm:ss.SSS"))
 }
 
 const createListOfOperations = (folder, options) => {
@@ -153,8 +84,29 @@ const performOperations = async ({ proxyFolder, proxySubfolders, proxies, copies
     }
     for (let i = 0; i < copies.length; i++) {
         const element = copies[i];
-        
+        await fs.copyFileSync(element.sourcePath, element.destinationPath)
     }
+}
+
+var transcodeFile = async function(file, proxyPath, options){
+    await cp.spawnSync('ffmpeg', [
+      '-i', file,
+      '-c:v', 'libx264',
+      '-pix_fmt', 'yuv420p',
+      // '-vf', ('scale='+ outputWidth +':'+outputHeight ),
+      '-preset', 'slow',
+      '-crf', (options && options.crfVal) ? options.crfVal : '23',
+      '-ac', '2',
+      '-c:a', 'aac',
+      '-b:a', '128k',
+      proxyPath
+    ], {
+      stdio: [
+        0, // Use parent's stdin for child
+        'pipe', // Pipe child's stdout to parent
+        2 // Direct child's stderr to a file
+      ]
+    });
 }
 
 // module.exports.transcodeFile = transcodeFile;
